a8ba931bb6278815d03be68b61031dd031a58a0f
Anatoly Svichkarev <svichkarev.anatoly@gmail.com>
2019-11-04 13:07:03 +0300

    done 1.2 => 2.1 - 2.6

---
+++ section3261/lesson35224/step146201/src/Main.java
@@ -0,0 +1,8 @@
+(x,y)->
+        {
+            long product = x;
+        for(long i=x+1;i<=y;i++){
+            product *= i;
+        }
+        return product;
+        };
---
+++ section3261/lesson42581/step173386/src/Main.java
@@ -0,0 +1,2 @@
+(x1, x2, x3, x4, x5, x6, x7) ->
+        ((String)(x1 + x2 + x3 + x4 + x5 + x6 + x7)).toUpperCase();
